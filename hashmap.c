88566cd00eb6aa141e65ab627559acda168adc3a
Replit user <>
2022-09-09 17:47:07 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -87,17 +87,13 @@ void eraseMap(HashMap * map,  char * key) {
       eliminate->key = NULL;
       map->size--;
     }
-    
-
 }
 
 Pair * searchMap(HashMap * map,  char * key) { 
     size_t i = hash(key, map->capacity);
     size_t cont = 0;
 
-    while(1) {
-      if(!map->buckets[i] || !map->buckets[i]->key) return NULL;
-      
+    while(!map->buckets[i] || !map->buckets[i]->key) {
       if(is_equal(key, map->buckets[i]->key)) {
           map->current = i;
           return map->buckets[i];
@@ -109,12 +105,11 @@ Pair * searchMap(HashMap * map,  char * key) {
       if(cont == map->capacity) break;
       if(i == map->capacity) i = 0;
     }
-
     return NULL;
 }
 
 Pair * firstMap(HashMap * map) {
-
+    Pair * first = searchMap(map, key);
     return NULL;
 }
 
