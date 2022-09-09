42ca4c6607a4c471003f496bbaca5a8f700106a3
Replit user <>
2022-09-09 17:26:58 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -87,7 +87,7 @@ void eraseMap(HashMap * map,  char * key) {
 
 Pair * searchMap(HashMap * map,  char * key) { 
     size_t i = hash(key, map->capacity);
-    size_t cont;
+    size_t cont = 0;
 
     while(1) {
       if(!map->buckets[i] || !map->buckets[i]->key) return NULL;
