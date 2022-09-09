6a0a14f186ea3bad0d965639153bce97b11bacdc
Replit user <>
2022-09-09 13:10:01 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -44,7 +44,7 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
-    HashMap * pairs = createMap();
+    HashMap * pairs = createMap(1);
 
     Pair * p = (Pair *) malloc(sizeof(Pair));
 
